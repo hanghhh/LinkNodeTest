b834987b4e7c3add75c2afbb42e4b726f92881ad
hanghhh <liu_zih@163.com>
2021-01-03 21:33:54 +0800

    1

---
+++ src/main/java/node/ListNode.java
@@ -0,0 +1,11 @@
+package node;
+
+public class ListNode {
+    public int value;
+    public ListNode next;
+    public ListNode rand;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+}
--- src/main/java/node/NodeTest.java
+++ src/main/java/node/NodeTest.java
@@ -2,6 +2,10 @@ package node;
 
 public class NodeTest {
     public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        ListNode b = listNode;
+        listNode.value = 2;
+        System.out.println(b.value);
 
     }
 
@@ -10,13 +14,13 @@ public class NodeTest {
      * 思路：
      * 快慢指针，相交后快指针回到head，再次相交即使环点
      */
-    public Node getloopNode(Node head) {
+    public ListNode getloopNode(ListNode head) {
         //只要出现null，代表没有环，也保证快指针初始化时不会产生空指针
         if(head == null || head.next == null || head.next.next == null) {
             return null;
         }
-        Node s = head.next;
-        Node f = head.next.next;
+        ListNode s = head.next;
+        ListNode f = head.next.next;
         while (s != f) {
             if(s.next == null || f.next.next == null) {
                 return null;
@@ -31,6 +35,51 @@ public class NodeTest {
             f = f.next;
         }
         return s;
+    }
+
+    /**
+     * 相交链表
+     * 思路
+     * 1.不考虑空间复杂度使用hash
+     * 2.获取链表长度差值，
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) {
+            return null;
+        }
+        //如果两个链表尾部节点相同，则必然是某处相交
+        ListNode curA = headA; //代表较长得数组
+        ListNode curB = headB;
+        int lengthA = 0; //链表长度差值
+        int lengthB = 0; //链表长度差值
+        //获取A链表尾部节点
+        while (curA.next != null) {
+            lengthA++;
+            curA = curA.next;
+        }
+        //获取B链表尾部节点
+        while (curB.next != null) {
+            lengthB++;
+            curB = curB.next;
+        }
+        if(curA != curB) {
+            return null;
+        }
+
+        //n得正负代表数组长度比较结果
+        int n = lengthA -lengthB;
+        curA = n > 0 ? headA : headB; //长数组头部
+        curB = curA == headA ? headB : headA;
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            curA = curA.next;
+        }
+        while (curA != curB) {
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return curA;
     }
 
 }
